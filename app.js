8a5bdfb261a343143b8fe4f8d415a84a913fce3c
= <=>
2020-08-30 03:32:08 -0300

    Add style

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const curriculum = require('./routes/curriculum')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/', index)
 app.use('/curriculum', curriculum)
 
